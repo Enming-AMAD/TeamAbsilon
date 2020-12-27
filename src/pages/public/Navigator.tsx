5714393e6bddd4a2577b1d1dc93c3ab952ccb51e
dacoonkr <dacoon0621@gmail.com>
2020-12-27 19:00:45 +0900

    Updates

---
+++ src/pages/public/Navigator.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Link } from 'react-router-dom';
+
+const names = [
+    <Link to="/" className="tab">메인</Link>,
+    <Link to="/project" className="tab">프로젝트</Link>,
+    <Link to="/tos" className="tab">이용약관</Link>,
+    <Link to="/privacy" className="tab">개인정보 처리방침</Link>,
+]
+const tabs = names.map((name) =>
+    <li>{name}</li>
+)
+
+export default tabs
